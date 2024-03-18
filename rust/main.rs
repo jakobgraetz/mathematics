e872d3e81bbabb7bcad959009156f2133db650bb
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-18 18:50:45 +0100

    [DEV] implemented basic functions in rust

---
+++ rust/main.rs
@@ -0,0 +1,25 @@
+// main.py
+// author Jakob Grätz (@jakobgraetz)
+// copyright Jakob Grätz (@jakobgraetz)
+// version 18/03/2024 (DD/MM/YYYY)
+// description Implementation of my maths repository in Python.
+
+fn simple_sum (first_summand: f64, second_summand: f64) -> f64 {
+    return first_summand + second_summand;
+}
+
+fn simple_difference (minuend: f64, subtrahend: f64) -> f64 {
+    return minuend - subtrahend;
+}
+
+fn simple_product (first_factor: f64, second_factor: f64) -> f64 {
+    return first_factor * second_factor;
+}
+
+fn simple_quotient (numerator: f64, denominator: f64) -> f64 {
+    return numerator / denominator;
+}
+
+fn main () {
+    
+}
