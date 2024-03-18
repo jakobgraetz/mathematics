dbf3e01e5c0937c8179f39a64edaaf45aeb3c0bb
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-18 18:33:19 +0100

    [DEV] implemented basic functions in swift

---
+++ swift/main.swift
@@ -0,0 +1,21 @@
+// main.swift
+// author Jakob Grätz (@jakobgraetz)
+// copyright Jakob Grätz (@jakobgraetz)
+// version 18/03/2024 (DD/MM/YYYY)
+// description Implementation of my maths repository in Swift.
+
+func simple_sum(first_summand: Float64, second_summand: Float64) -> Float64 {
+    return first_summand + second_summand
+}
+
+func simple_difference(minuend: Float64, subtrahend: Float64) -> Float64 {
+    return minuend - subtrahend
+}
+
+func simple_product(first_factor: Float64, second_factor: Float64) -> Float64 {
+    return first_factor * second_factor
+}
+
+func simple_quotient (numerator: Float64, denominator: Float64) -> Float64 {
+    return numerator / denominator
+}
