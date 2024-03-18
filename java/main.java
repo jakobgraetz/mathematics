40ce0d977c1552078a82b8650d2d613680ba099d
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-18 18:44:10 +0100

    [DEV] implemented basic functions in java

---
+++ java/main.java
@@ -0,0 +1,27 @@
+// main.java
+// author Jakob Grätz (@jakobgraetz)
+// copyright Jakob Grätz (@jakobgraetz)
+// version 18/03/2024 (DD/MM/YYYY)
+// description Implementation of my maths repository in Java.
+
+public class main {
+    public static void main (String[] Args) {
+        
+    }
+
+    public static Double simple_sum (Double first_summand, Double second_summand) {
+        return first_summand + second_summand;
+    }
+
+    public static Double simple_difference (Double minuend, Double subtrahend) {
+        return minuend - subtrahend;
+    }
+
+    public static Double simple_product (Double first_factor, Double second_factor) {
+        return first_factor * second_factor;
+    }
+    
+    public static Double simple_quotient (Double numerator, Double denominator) {
+        return numerator / denominator;
+    }
+}
