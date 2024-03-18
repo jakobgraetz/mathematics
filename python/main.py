5044e0dbb35bad6610c771784a5d815a2595c4ad
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-18 18:27:38 +0100

    [DEV] implemented basic functions in  python

---
+++ python/main.py
@@ -0,0 +1,17 @@
+# main.py
+# author Jakob Grätz (@jakobgraetz)
+# copyright Jakob Grätz (@jakobgraetz)
+# version 18/03/2024 (DD/MM/YYYY)
+# description Implementation of my maths repository in Python.
+
+def simple_sum(first_summand: float, second_summand: float):
+    return first_summand + second_summand
+
+def simple_difference(minuend: float, subtrahend: float):
+    return minuend - subtrahend
+
+def simple_product(first_factor: float, second_factor: float):
+    return first_factor * second_factor
+
+def simple_quotient (numerator: float, denominator: float):
+    return numerator / denominator
