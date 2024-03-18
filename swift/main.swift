38531676b1ad80389a93277e070f63be1178491d
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-18 18:51:50 +0100

    [DEV] improved code readability

--- swift/main.swift
+++ swift/main.swift
@@ -4,15 +4,15 @@
 // version 18/03/2024 (DD/MM/YYYY)
 // description Implementation of my maths repository in Swift.
 
-func simple_sum(first_summand: Float64, second_summand: Float64) -> Float64 {
+func simple_sum (first_summand: Float64, second_summand: Float64) -> Float64 {
     return first_summand + second_summand
 }
 
-func simple_difference(minuend: Float64, subtrahend: Float64) -> Float64 {
+func simple_difference (minuend: Float64, subtrahend: Float64) -> Float64 {
     return minuend - subtrahend
 }
 
-func simple_product(first_factor: Float64, second_factor: Float64) -> Float64 {
+func simple_product (first_factor: Float64, second_factor: Float64) -> Float64 {
     return first_factor * second_factor
 }
 
