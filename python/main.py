55d415c62eb88fe654f8b5f19014a693fb412d0d
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-20 06:50:06 +0100

    [DEV] implemented division by zero error in python

--- python/main.py
+++ python/main.py
@@ -4,6 +4,9 @@
 # version 19/03/2024 (DD/MM/YYYY)
 # description Implementation of my maths repository in Python.
 
+class DivisionByZeroError(Exception):
+    pass
+
 def simple_sum(first_summand: float, second_summand: float):
     return first_summand + second_summand
 
@@ -14,4 +17,7 @@ def simple_product(first_factor: float, second_factor: float):
     return first_factor * second_factor
 
 def simple_quotient (numerator: float, denominator: float):
-    return numerator / denominator
+    if denominator != 0:
+        return numerator / denominator
+    else:
+        raise DivisionByZeroError("Division by zero is not allowed")
