a4b5ecaf1d1197abd7c67e00d951e721c76219c3
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-20 06:35:57 +0100

    [DEV] implemented basic functionality in C++

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,6 +6,22 @@
 // version 19/03/2024 (DD/MM/YYYY)
 // description Implementation of my maths repository in C++.
 
+double simple_sum(double first_summand, double second_summand) {
+    return first_summand + second_summand;
+}
+
+static double simple_difference(double minuend, double subtrahend) {
+    return minuend - subtrahend;
+}
+
+static double simple_product(double first_factor, double second_factor) {
+    return first_factor * second_factor;
+}
+
+static double simple_quotient(double numerator, double denominator) {
+    return numerator / denominator;
+}
+
 int main () {
     return 0;
 }
