bb7e26413ef6b3c81d7074cc7e7c1cea0e2de2f7
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-20 06:57:33 +0100

    [DEV] implemented division by zero error in c++

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <stdexcept>
 
 // main.cpp
 // author Jakob Grätz (@jakobgraetz)
 // copyright 2024 - Jakob Grätz (@jakobgraetz)
 // version 19/03/2024 (DD/MM/YYYY)
 // description Implementation of my maths repository in C++.
+
+class DivisionByZeroException : public std::exception {
+public:
+    const char* what() const _NOEXCEPT override {
+        return "Division by zero is not allowed";
+    }
+};
 
 double simple_sum(double first_summand, double second_summand) {
     return first_summand + second_summand;
@@ -19,7 +27,11 @@ static double simple_product(double first_factor, double second_factor) {
 }
 
 static double simple_quotient(double numerator, double denominator) {
-    return numerator / denominator;
+    if (denominator != 0) {
+        return numerator / denominator;
+    } else {
+        throw DivisionByZeroException();
+    }
 }
 
 int main () {
