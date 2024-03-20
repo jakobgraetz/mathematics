0e44e9b62cd1b97fae96c4d97cc65a6780daaf2f
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-20 06:30:03 +0100

    [DEV] began implementing in  C++

---
+++ cpp/main.cpp
@@ -0,0 +1,9 @@
+// main.cpp
+// author Jakob Grätz (@jakobgraetz)
+// copyright 2024 - Jakob Grätz (@jakobgraetz)
+// version 19/03/2024 (DD/MM/YYYY)
+// description Implementation of my maths repository in C++.
+
+int main () {
+    return 0;
+}
