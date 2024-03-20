4d63fa589bbfb638f2ed81349c03e180757f8f51
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-20 06:48:01 +0100

    [DEV] implemented division by zero error in Rust

--- rust/main.rs
+++ rust/main.rs
@@ -4,6 +4,11 @@
 // version 19/03/2024 (DD/MM/YYYY)
 // description Implementation of my maths repository in Rust.
 
+#[derive(Debug)]
+enum DivisionError {
+    DivisionByZero,
+}
+
 fn simple_sum (first_summand: f64, second_summand: f64) -> f64 {
     return first_summand + second_summand;
 }
@@ -16,8 +21,12 @@ fn simple_product (first_factor: f64, second_factor: f64) -> f64 {
     return first_factor * second_factor;
 }
 
-fn simple_quotient (numerator: f64, denominator: f64) -> f64 {
-    return numerator / denominator;
+fn simple_quotient (numerator: f64, denominator: f64) -> Result<f64, DivisionError> {
+    if denominator != 0.0 {
+        Ok(numerator / denominator)
+    } else {
+        Err(DivisionError::DivisionByZero)
+    }
 }
 
 fn main () {
