b08e388ce61c0d97ca5e46133b7c11c0d6e925ef
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-21 06:37:45 +0100

    [DEV] added comments

--- swift/main.swift
+++ swift/main.swift
@@ -4,22 +4,27 @@
 // version 19/03/2024 (DD/MM/YYYY)
 // description Implementation of my maths repository in Swift.
 
+// Exception for Handling Division by Zero
 enum DivisionError: Error {
     case divisionByZero
 }
 
+// Sum Function
 func simple_sum (first_summand: Float64, second_summand: Float64) -> Float64 {
     return first_summand + second_summand
 }
 
+// Difference Function
 func simple_difference (minuend: Float64, subtrahend: Float64) -> Float64 {
     return minuend - subtrahend
 }
 
+// Product Function
 func simple_product (first_factor: Float64, second_factor: Float64) -> Float64 {
     return first_factor * second_factor
 }
 
+// Division Function
 func simple_quotient (numerator: Float64, denominator: Float64) throws -> Float64 {
     guard denominator != 0 else {
         throw DivisionError.divisionByZero
