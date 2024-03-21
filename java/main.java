afa6b454391a1496aac83da3a31a425370ee27be
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-21 06:34:29 +0100

    [DEV] added doc comments

--- java/main.java
+++ java/main.java
@@ -6,6 +6,7 @@ package java;
 // version 19/03/2024 (DD/MM/YYYY)
 // description Implementation of my maths repository in Java.
 
+// Exception for Handling Division by Zero
 class DivisionByZeroException extends Exception {
     public DivisionByZeroException(String message) {
         super(message);
@@ -13,22 +14,27 @@ class DivisionByZeroException extends Exception {
 }
 
 public class main {
+    // Main Method
     public static void main (String[] Args) {
         
     }
 
+    // Sum Method
     public static Double simple_sum (Double first_summand, Double second_summand) {
         return first_summand + second_summand;
     }
 
+    // Difference Method
     public static Double simple_difference (Double minuend, Double subtrahend) {
         return minuend - subtrahend;
     }
 
+    // Product Method
     public static Double simple_product (Double first_factor, Double second_factor) {
         return first_factor * second_factor;
     }
     
+    // Division Method
     public static Double simple_quotient (Double numerator, Double denominator) throws DivisionByZeroException {
         if (denominator != 0.0) {
             return numerator / denominator;
