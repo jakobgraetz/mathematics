f949dca9d0a310c561a1f6358816944302df2078
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-21 06:35:54 +0100

    [DEV] added doc comments

--- python/main.py
+++ python/main.py
@@ -4,18 +4,23 @@
 # version 19/03/2024 (DD/MM/YYYY)
 # description Implementation of my maths repository in Python.
 
+# Exception for Handling Division by Zero
 class DivisionByZeroError(Exception):
     pass
 
+# Sum Function
 def simple_sum(first_summand: float, second_summand: float):
     return first_summand + second_summand
 
+# Difference Function
 def simple_difference(minuend: float, subtrahend: float):
     return minuend - subtrahend
 
+# Product Function
 def simple_product(first_factor: float, second_factor: float):
     return first_factor * second_factor
 
+# Division Function
 def simple_quotient (numerator: float, denominator: float):
     if denominator != 0:
         return numerator / denominator
