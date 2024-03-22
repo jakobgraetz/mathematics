8b547d95278f75a893c16c40c6a02c70d26944a5
WafflyOwl1 <96118933+WafflyOwl1@users.noreply.github.com>
2024-03-22 06:59:50 +0100

    [DOC] updated disclaimer

--- swift/main.swift
+++ swift/main.swift
@@ -1,7 +1,7 @@
 // main.swift
 // author Jakob Grätz (@jakobgraetz)
 // copyright 2024 - Jakob Grätz (@jakobgraetz)
-// version 19/03/2024 (DD/MM/YYYY)
+// version 22/03/2024 (DD/MM/YYYY)
 // description Implementation of my maths repository in Swift.
 
 // Exception for Handling Division by Zero
